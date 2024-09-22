4a94b927d86a138dc008ebe1a22886beabf8a996
Muhammad Javier <84699356+muhajav@users.noreply.github.com>
2024-09-22 14:12:48 +0700

    Update main.cpp

--- problem2/main.cpp
+++ problem2/main.cpp
@@ -1,1 +1,30 @@
-hbbjbjb
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	float a, b, c;
+
+	cin>>a>>b>>c;
+
+	float D = b*b - 4*a*c;
+
+	if(D<0)
+	{
+		cout<<"The equation has no solution"<<'\n';
+	} else if(D==0.0)
+	{
+		float x = (-b)/2*a;
+		cout<<"x: "<<x<<'\n';
+	} else {
+		float x1 = ((b*-1) + sqrt(D))/2*a;
+		float x2 = ((b*-1) - sqrt(D))/2*a;
+
+		cout<<"x1: "<<x1<<'\n';
+		cout<<"x2: "<<x2<<'\n';
+	}
+	
+}
